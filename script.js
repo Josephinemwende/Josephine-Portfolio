135d0bb889c81d5ffcd148f1ae35d44a5b25978f
Josephine Mwende <josymwesh45@gmail.com>
2025-09-25 07:27:54 +0300

    portfolio

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+/*document.addEventListener("DOMContentLoaded", () => {
   const toggleBtn = document.getElementById("darkModeToggle");
 
   if (!toggleBtn) return; // Safety check
@@ -20,4 +20,50 @@ document.addEventListener("DOMContentLoaded", () => {
       toggleBtn.textContent = "🌙 Dark Mode";
     }
   });
+});*/
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault(); // stop the # from showing in URL
+      const targetId = link.getAttribute('href').substring(1);
+      const target = document.getElementById(targetId);
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+
+const scrollBtn = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    scrollBtn.classList.remove("d-none");
+    scrollBtn.src = "Images/icons8-up-48.png"; // change to UP arrow
+  } else {
+    scrollBtn.classList.remove("d-none");
+    scrollBtn.src = "Images/icons8-down-48.png"; // change to DOWN arrow
+  }
 });
+
+scrollBtn.addEventListener("click", () => {
+  if (window.scrollY > 200) {
+    window.scrollTo({ top: 0, behavior: "smooth" }); // scroll up
+  } else {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" }); // scroll down
+  }
+});
+
+
+document.querySelectorAll(".progress-demo").forEach(btn => {
+    btn.addEventListener("click", e => {
+      e.preventDefault();
+      btn.innerHTML = '<span class="spinner-border spinner-border-sm me-2"></span> In Progress…';
+      btn.classList.add("disabled");
+
+      // reset after 3s
+      setTimeout(() => {
+        btn.innerHTML = '<i class="bi bi-box-arrow-up-right me-1"></i> Live Demo';
+        btn.classList.remove("disabled");
+      }, 3000);
+    });
+  });
